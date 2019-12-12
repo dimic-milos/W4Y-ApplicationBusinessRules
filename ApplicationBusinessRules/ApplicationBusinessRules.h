1792010e857c7bf91627abdefa8ddba4c44877cf
Milos Dimic <dimic.milos@icloud.com>
2019-12-12 10:27:35 +0100

    Initial Commit

---
+++ ApplicationBusinessRules/ApplicationBusinessRules.h
@@ -0,0 +1,19 @@
+//
+//  ApplicationBusinessRules.h
+//  ApplicationBusinessRules
+//
+//  Created by Dimic Milos on 12/12/19.
+//  Copyright © 2019 Dimic Milos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ApplicationBusinessRules.
+FOUNDATION_EXPORT double ApplicationBusinessRulesVersionNumber;
+
+//! Project version string for ApplicationBusinessRules.
+FOUNDATION_EXPORT const unsigned char ApplicationBusinessRulesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApplicationBusinessRules/PublicHeader.h>
+
+
